b228f444ffdc6ab30826c2c1524d4bcbf8d00639
NathanPradelle <nathan.pradelle77@gmail.com>
2024-01-11 21:58:48 +0100

    add timer, fix scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,150 +22,181 @@ public class Player : MonoBehaviour
     public Text scoreText;
     public MenuController MenuController;
     private bool sprintUtilisable;
-    
+    public float Timer;
+    public Text TimerText;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         endurance = enduranceMax;
+        Timer = 0;
     }
-    
-    public void Highest() 
+
+    public void Highest()
     {
-        
+        PlayerPrefs.SetFloat("Time", Timer);
         PlayerPrefs.SetInt("score", pourcent);
-        if (PlayerPrefs.GetInt("highscore" + MenuController.Lvlname) < pourcent || PlayerPrefs.GetInt("highscore" + MenuController.Lvlname) == 0)
+        PlayerPrefs.Save();
+        if (PlayerPrefs.GetInt("highscore" + MenuController.Lvlname) < PlayerPrefs.GetInt("score") ||
+            PlayerPrefs.GetInt("highscore" + MenuController.Lvlname) == 0)
         {
             PlayerPrefs.SetInt("highscore" + MenuController.Lvlname, pourcent);
+            PlayerPrefs.Save();
         }
+
+        if (PlayerPrefs.GetFloat("Timer" + MenuController.Lvlname) > PlayerPrefs.GetFloat("Time") ||
+            PlayerPrefs.GetFloat("Timer" + MenuController.Lvlname) == 0)
+        {
+            PlayerPrefs.SetFloat("Timer" + MenuController.Lvlname, Timer);
+            PlayerPrefs.Save();
+        }
+        
     }
 
     // Update is called once per frame
     private void Update()
-    {   
+    {
+        Timer += Time.deltaTime;
+        TimerText.text = Timer.ToString("F1");
         
         pourcent = Mathf.RoundToInt(bodyTransform.transform.position.y / finish.transform.position.y * 100);
         scoreText.text = pourcent.ToString() + "%";
-        
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            directionIntent += Vector3.forward;
-        }
 
-        if (Input.GetKey(KeyCode.S))
         {
-            directionIntent += Vector3.back;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            directionIntent += Vector3.left;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            directionIntent += Vector3.right;
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift)  &&
-            Physics.SphereCast(bodyTransform.position + Vector3.up * (0.1f + 0.45f), 0.45f, Vector3.down, 
-                out var Info, 
-                    0.11f)
-            )
-        {
-            if (sprintUtilisable) {
-                sprintBoost = sprintAcceleration;
-                if (endurance >= 0) { 
-                endurance -= 0.1f;
-                } else {
-                    sprintUtilisable = false;
-                }
-
-            } else {
-                sprintBoost = 1;
+            if (Input.GetKey(KeyCode.W))
+            {
+                directionIntent += Vector3.forward;
             }
 
+            if (Input.GetKey(KeyCode.S))
+            {
+                directionIntent += Vector3.back;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                directionIntent += Vector3.left;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                directionIntent += Vector3.right;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift) &&
+                Physics.SphereCast(bodyTransform.position + Vector3.up * (0.1f + 0.45f), 0.45f, Vector3.down,
+                    out var Info,
+                    0.11f)
+               )
+            {
+                if (sprintUtilisable)
+                {
+                    sprintBoost = sprintAcceleration;
+                    if (endurance >= 0)
+                    {
+                        endurance -= 0.1f;
+                    }
+                    else
+                    {
+                        sprintUtilisable = false;
+                    }
+
+                }
+                else
+                {
+                    sprintBoost = 1;
+                }
 
 
-            
-            
-        } else {
-            sprintBoost = 1;
-            
-            if (endurance < enduranceMax) {
+
+
+
+            }
+            else
+            {
+                sprintBoost = 1;
+
+                if (endurance < enduranceMax)
+                {
                     endurance += 0.1f;
-                } else {
+                }
+                else
+                {
                     sprintUtilisable = true;
                 }
-                
+
             }
-        
-        //Debug.Log("Endurance: " + endurance);
 
-        var mouseXDelta = Input.GetAxis("Mouse X");
+            //Debug.Log("Endurance: " + endurance);
 
-        bodyTransform.Rotate(Vector3.up, Time.deltaTime * yawRotationSpeed * mouseXDelta);
+            var mouseXDelta = Input.GetAxis("Mouse X");
 
-        var mouseYDelta = Input.GetAxis("Mouse Y");
+            bodyTransform.Rotate(Vector3.up, Time.deltaTime * yawRotationSpeed * mouseXDelta);
 
-        var rotation = cameraTransform.localRotation;
+            var mouseYDelta = Input.GetAxis("Mouse Y");
 
-        var rotationX = rotation.eulerAngles.x;
+            var rotation = cameraTransform.localRotation;
 
-        rotationX += -Time.deltaTime * pitchRotationSpeed * mouseYDelta;
-        
+            var rotationX = rotation.eulerAngles.x;
 
-        var unClampedRotationX = rotationX;
+            rotationX += -Time.deltaTime * pitchRotationSpeed * mouseYDelta;
 
-        if (unClampedRotationX >= 180)
-        {
-            unClampedRotationX -= 360;
-        }
 
-        var clampedRotationX = Mathf.Clamp(unClampedRotationX, -60, 60);
+            var unClampedRotationX = rotationX;
 
-        cameraTransform.localRotation =
-            Quaternion.Euler(new Vector3(
-                clampedRotationX,
-                rotation.eulerAngles.y,
-                rotation.eulerAngles.z
-            ));
+            if (unClampedRotationX >= 180)
+            {
+                unClampedRotationX -= 360;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space) &&
-            Physics.SphereCast(bodyTransform.position + Vector3.up * (0.1f + 0.45f), 0.45f, Vector3.down, 
-                out var _hitInfo, 
+            var clampedRotationX = Mathf.Clamp(unClampedRotationX, -60, 60);
+
+            cameraTransform.localRotation =
+                Quaternion.Euler(new Vector3(
+                    clampedRotationX,
+                    rotation.eulerAngles.y,
+                    rotation.eulerAngles.z
+                ));
+
+            if (Input.GetKeyDown(KeyCode.Space) &&
+                Physics.SphereCast(bodyTransform.position + Vector3.up * (0.1f + 0.45f), 0.45f, Vector3.down,
+                    out var _hitInfo,
                     0.11f)
-            )
-        {
-            wantToJump = true;
+               )
+            {
+                wantToJump = true;
+            }
         }
+
     }
-    
+
     private void FixedUpdate()
-    {
-        var normalizedDirection = directionIntent.normalized;
-        bodyTransform.position += bodyTransform.rotation * normalizedDirection * (Time.deltaTime * speed * sprintBoost);
-        lava.position += Vector3.up.normalized * (Time.deltaTime / 2);
-
-        directionIntent = Vector3.zero;
-        
-
-        if (wantToJump)
         {
-            playerRigidBody.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
-            wantToJump = false;
-            DetachFromPlatform();
-        }
+            var normalizedDirection = directionIntent.normalized;
+            bodyTransform.position +=
+                bodyTransform.rotation * normalizedDirection * (Time.deltaTime * speed * sprintBoost);
+            lava.position += Vector3.up.normalized * (Time.deltaTime / 2);
 
-        if (directionIntent != Vector3.zero)
-        {
-            Vector3 moveDirection = bodyTransform.rotation * directionIntent.normalized * speed * sprintBoost;
-            playerRigidBody.MovePosition(playerRigidBody.position + moveDirection * Time.deltaTime);
             directionIntent = Vector3.zero;
+
+
+            if (wantToJump)
+            {
+                playerRigidBody.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
+                wantToJump = false;
+                DetachFromPlatform();
+            }
+
+            if (directionIntent != Vector3.zero)
+            {
+                Vector3 moveDirection = bodyTransform.rotation * directionIntent.normalized * speed * sprintBoost;
+                playerRigidBody.MovePosition(playerRigidBody.position + moveDirection * Time.deltaTime);
+                directionIntent = Vector3.zero;
+            }
+        }
+
+        private void DetachFromPlatform()
+        {
+            transform.SetParent(null);
         }
     }
-        private void DetachFromPlatform()
-    {
-        transform.SetParent(null);
-    }
-}
---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    
+    public Text highestTimeText;
+
+    public Text TimeText;
+    public MenuController MenuController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeText.text = "Your Time : " + PlayerPrefs.GetFloat("Time").ToString("F2");
+        highestTimeText.text = "Best time : " + PlayerPrefs.GetFloat("Timer" + MenuController.Lvlname).ToString("F2");
+    }
+    
+}
